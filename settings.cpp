c4f8923a1d39dc315b298ed8acf63eff3a607491
Marc Clint Dion <marcclintdion@Marcs-iMac.local>
2015-09-17 22:07:02 -0700

    “init”

---
+++ settings.cpp
@@ -0,0 +1,12 @@
+var         shinyPlasticFireBall_SECOND_SPECULAR[]    =  {1.37, 1, 1.17, 1.0};
+
+var         colorRamp_RAMP_VALUE_XY               =  0, 0.239;
+
+var         chromatic_dispersion_ATTENUATION      =  0.095;
+var         chromatic_dispersion_SHININESS        =  2.94595;
+var         chromatic_dispersion_adjustment       =  0.952;
+
+
+          moveSet[0]    =  -2.1;
+          moveSet[1]    =  1.9;
+          moveSet[2]    =  -8.3;
